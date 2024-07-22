a54e827589652a39883877894f90fbb94c8086a3
samuel udoka <shedrachsamuel4@gmail.com>
2024-07-22 13:22:00 +0100

    commit after updating payment controller

--- src/main/java/com/gosave/gosave/controller/PaymentServiceController.java
+++ src/main/java/com/gosave/gosave/controller/PaymentServiceController.java
@@ -1,13 +1,20 @@
 package com.gosave.gosave.controller;
 
+import com.gosave.gosave.dto.request.WalletRequest;
+import com.gosave.gosave.dto.response.ApiResponse;
 import com.gosave.gosave.dto.response.TransactionHistoryResponse;
+import com.gosave.gosave.dto.response.WalletResponse;
 import com.gosave.gosave.exception.UserException;
+import com.gosave.gosave.exception.UserNotFoundException;
+import com.gosave.gosave.exception.WalletExistException;
 import com.gosave.gosave.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PaymentServiceController {
     @Autowired
     private PaymentService paymentService;
+
+
 
     @GetMapping("/transactionHistory")
     ResponseEntity<TransactionHistoryResponse>getTransactionHistory() throws UserException {
